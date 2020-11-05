a6267548bbc744b6fbaea34fa1d6057f4f1bf5ad
THARAK <tharak1991@gmail.com>
2020-11-05 11:49:38 +0530

    updates

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ let User = require('./data/user');
 let userData = require('./data/user-data');
 let messageService = require('./service/message.service');
 
+const port = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(express.static('public'));
 
-const server = app.listen(5000, function () {
-  console.log('Listening to port 5000.');
+const server = app.listen(port, function () {
+  console.log('Listening to port' + port );
 });
 
 const io = socket(server);
